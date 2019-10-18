42a2d4dbdff3f6e767cb1f0495ee8aa21a08b93f
Jesper Nyhlen <jeppe_nyhlen@hotmail.com>
2019-10-18 02:31:22 +0200

    Adding chat feature after repo clean

---
+++ routes/chat.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const chat = require('../models/chat.js');
+
+router.post('/insert', (req, res) => {
+    chat.insertMessage(req.body, res);
+});
+
+router.get('/list', (req, res) => {
+    chat.getMessages(req, res);
+});
+
+module.exports = router;
