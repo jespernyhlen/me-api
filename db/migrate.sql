8fae122c96906c44a44457b9f6418de91fb7b949
Jesper Nyhlen <jeppe_nyhlen@hotmail.com>
2019-10-27 16:14:16 +0100

    Adding reset script for db

--- db/migrate.sql
+++ db/migrate.sql
@@ -3,22 +3,36 @@ DROP TABLE IF EXISTS 'reports';
 DROP TABLE IF EXISTS 'presentation';
 
 
-CREATE TABLE IF NOT EXISTS users
+CREATE TABLE
+IF NOT EXISTS users
 (
-    email VARCHAR(255) PRIMARY KEY NOT NULL,
-    firstname VARCHAR(255) NOT NULL,
-    lastname VARCHAR(255) NOT NULL,
-    birthday VARCHAR(20) NOT NULL,
-    password VARCHAR (60) NOT NULL,
-    UNIQUE(email)
+    email VARCHAR
+(255) PRIMARY KEY NOT NULL,
+    firstname VARCHAR
+(255) NOT NULL,
+    lastname VARCHAR
+(255) NOT NULL,
+    birthday VARCHAR
+(20) NOT NULL,
+    password VARCHAR
+(60) NOT NULL,
+    UNIQUE
+(email)
 );
 
-CREATE TABLE IF NOT EXISTS reports (
+CREATE TABLE
+IF NOT EXISTS reports
+(
     kmom INT PRIMARY KEY NOT NULL,
-    text VARCHAR(1000)
+    text VARCHAR
+(1000)
 );
 
-CREATE TABLE IF NOT EXISTS presentation (
-    name VARCHAR(20) NOT NULL,
-    text VARCHAR (500) NOT NULL
+CREATE TABLE
+IF NOT EXISTS presentation
+(
+    name VARCHAR
+(20) NOT NULL,
+    text VARCHAR
+(500) NOT NULL
 );
